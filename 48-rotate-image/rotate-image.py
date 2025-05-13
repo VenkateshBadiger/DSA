7471f2aa4052ab7c210515bff68a0be4a643b5ce
Venkatesh <159639777+VenkateshBadiger@users.noreply.github.com>
2025-05-13 16:45:18 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (66.61%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -7,7 +7,7 @@ class Solution:
 
         mat=[[0]*n for _ in range(n)]
 
-        for i in range(n-1,-1,-1):
+        for i in range(n):
             for j in range(n):
                 mat[j][n-1-i]=matrix[i][j]
 
