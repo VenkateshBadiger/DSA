3c10a246681c668d78b8f1a6c650412220badc30
Venkatesh <159639777+VenkateshBadiger@users.noreply.github.com>
2025-09-10 13:52:56 +0530

    Time: 10 ms (54.88%) | Memory: 18.8 MB (87.85%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.py
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.py
@@ -1,6 +1,7 @@
 class Solution:
     def largestOddNumber(self, num: str) -> str:
-        
+        # RETURNING THE LARGEST POSSIBLE ODD INTEGER 
+        # START TRAVERSING FROM RIGHT -> LEFT 
         
         for i in range(len(num)-1, -1, -1):
             if int(num[i]) % 2 == 1:
