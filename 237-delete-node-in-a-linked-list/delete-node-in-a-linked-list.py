bf8f523567abcf649d93ff0bdeb51dafd658c938
Venkatesh <159639777+VenkateshBadiger@users.noreply.github.com>
2025-09-13 15:37:58 +0530

    Time: 46 ms (24.69%) | Memory: 17.8 MB (99.24%) - LeetSync

---
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        node.val = node.next.val
+        node.next= node.next.next
+        
+
+        # The sol u wrote is when u have the head, here the question has not given u the head only the node  
+        """
+        prev = head
+        curr = head.next 
+        if prev.val == node.val:
+            head = head.next
+        
+        while curr is not None:
+            if curr.val == node.val:
+                prev.next = curr.next
+                curr.next = None
+            prev =curr
+            curr= curr.next
+        return head
+        """
