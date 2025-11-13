b7293330225fb5b216049e37c9f2c41cd2bc99af
Venkatesh <159639777+VenkateshBadiger@users.noreply.github.com>
2025-11-14 02:55:19 +0530

    Time: 405 ms (47.76%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,4 +1,12 @@
+SELECT author_id as id
+from Views
+where author_id =  viewer_id
+GROUP BY author_id
+order by author_id
+
+/*
 SELECT DISTINCT author_id as id
 from Views
 where author_id =  viewer_id
 order by author_id
+*/
