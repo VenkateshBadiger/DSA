eaeca111a6f3cdaf73094578593a4e431745f8ba
Venkatesh <159639777+VenkateshBadiger@users.noreply.github.com>
2025-11-30 13:34:52 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (59.92%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -13,16 +13,17 @@ class Solution:
         queue = deque([root])
         while queue:
             level_size = len(queue)
-            level = []
-            for _ in range(level_size):
+            
+            for i in range(level_size):
                 current = queue.popleft()
-                level.append(current.val)
+                
+                if i == level_size-1:
+                    res.append(current.val)
 
                 if current.left:
                     queue.append(current.left)
 
                 if current.right:
                     queue.append(current.right)
-                
-            res.append(level[-1])
+            
         return res
