8df8aecd3e10531649a8500914635a5eebb443cd
Venkatesh <159639777+VenkateshBadiger@users.noreply.github.com>
2025-12-12 23:09:58 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (41.23%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -21,27 +21,3 @@ class Solution:
                 l=m+1
             else:
                 r=m-1
-        '''
-        n=len(nums)
-        i=0
-        def rotated(nums:List[int])-> int:
-            for i in range(n-1):
-                if nums[i]>nums[i+1]:
-                    break 
-            return i 
-        k= rotated(nums)+1
-
-        a=nums[0:k]        
-        b=nums[k:n]
-        nums[:]=a+b
-        l=0
-        r=n-1
-        while l<=r:
-            m=l+((r-l)//2) 
-            if nums[m]==target:
-                return m
-            elif nums[m]<target:
-                l=m+1
-            else:
-                r=m-1
-        '''
