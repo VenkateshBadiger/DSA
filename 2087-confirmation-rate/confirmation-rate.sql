42729a38460a8177e514460e9913157dc21393d5
Venkatesh <159639777+VenkateshBadiger@users.noreply.github.com>
2026-07-06 17:26:10 +0530

    Time: 756 ms (24.42%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -0,0 +1,17 @@
+with req as 
+(
+    SELECT c.user_id, COUNT(*) as req
+    FROM Confirmations c
+    GROUP BY c.user_id
+),
+con_req as 
+(
+    SELECT c.user_id, COUNT(*) as con_req
+    FROM Confirmations c
+    WHERE c.action = 'confirmed'
+    GROUP BY c.user_id
+)
+SELECT s.user_id, ROUND(COALESCE(cr.con_req/r.req, 0),2) confirmation_rate
+FROM Signups s
+LEFT JOIN req r on r.user_id = s.user_id
+LEFT JOIN con_req cr on cr.user_id = s.user_id
