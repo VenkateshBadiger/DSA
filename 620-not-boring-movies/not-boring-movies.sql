5ef519496d8d1d0025b51ef29c6a02be2084cb51
Venkatesh <159639777+VenkateshBadiger@users.noreply.github.com>
2026-07-06 17:42:19 +0530

    Time: 271 ms (59.48%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,4 +1,4 @@
-SELECT * 
+SELECT *
 FROM Cinema c 
-WHERE c.id % 2 = 1 AND c.description != "boring"
-ORDER BY c.RATING DESC 
+WHERE c.id % 2 = 1 AND c.description NOT LIKE '%boring' 
+ORDER BY rating DESC
