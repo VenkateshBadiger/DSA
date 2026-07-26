5de1d24af73a1d704f48d3f146e34eba3f298fe4
Venkatesh <159639777+VenkateshBadiger@users.noreply.github.com>
2026-07-26 16:46:57 +0530

    Time: 3 ms (89.05%) | Memory: 20.1 MB (65.41%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n =len(nums)
+        maxpos = 0
+        steps = 0
+        end = 0
+        for i in range(n-1):
+            maxpos = max(maxpos, i+nums[i])
+            if i == end:
+                steps+=1
+                end = maxpos
+        return steps
+
+                
