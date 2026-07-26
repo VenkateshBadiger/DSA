74c721d57c2b10cbfb71ef359af242fcd5e4c010
Venkatesh <159639777+VenkateshBadiger@users.noreply.github.com>
2026-07-27 00:55:56 +0530

    Time: 3 ms (16.78%) | Memory: 19.4 MB (47.14%) - LeetSync

---
+++ 289-game-of-life/game-of-life.py
@@ -0,0 +1,24 @@
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        m = len(board)
+        n = len(board[0])
+        old = [row[:] for row in board]
+        directions = [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
+        for i in range(m):
+            for j in range(n):
+                lives = 0
+                for x,y in directions:
+                    ni = x + i 
+                    nj = y + j
+                    if 0<=ni<m and 0<=nj<n:
+                        if old[ni][nj] == 1:
+                            lives +=1
+                    
+                if  board[i][j]== 1:
+                    if lives < 2 or lives > 3:
+                        board[i][j] = 0
+                else:
+                    if lives == 3:
+                        board[i][j] = 1
+        return board
+                
